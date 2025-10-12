0abf562b47f7228e3cf136290449aa60f6fe715f
AimbotParce <marcparcerisa@gmail.com>
2025-10-13 00:50:46 +0200

    Improve custom tokenizer to use the paddings and proper vocab from the original one

--- recruitair/modeling/tokenize.py
+++ recruitair/modeling/tokenize.py
@@ -4,7 +4,7 @@
 from typing import List, Tuple
 
 import torch
-from transformers import PreTrainedTokenizer
+from transformers import BatchEncoding, PreTrainedTokenizer
 
 
 class ResumeAndCriteriaTokenizer:
@@ -17,35 +17,32 @@ class ResumeAndCriteriaTokenizer:
         Initialize the tokenizer for Resumes and their key criterias.
         """
         self.tokenizer = pretrained_tokenizer
-        self.eos_token_id = pretrained_tokenizer.eos_token_id
-        self.pad_token_id = pretrained_tokenizer.pad_token_id
+        self.eos_token = self.tokenizer.special_tokens_map.get("eos_token", None)
+        if self.eos_token is None:
+            raise ValueError("The tokenizer must have an EOS token.")
 
     def __call__(
-        self, resumes: List[str], criterias: List[str]
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        self,
+        resumes: List[str],
+        criterias: List[str],
+        padding: bool = False,
+        padding_side: str = "right",
+        return_tensors: str = "pt",
+    ) -> BatchEncoding:
         """
         Tokenize resumes and criterias separately, then concatenate them with
         EOS in between and at the end.
         """
-        # Join the resume and criteria tokens with "EOS" in between and at the end
-        resume_tokens = list(
-            map(lambda x: x + [self.eos_token_id], self.tokenizer(resumes)["input_ids"])
+        if len(resumes) != len(criterias):
+            raise ValueError("The number of resumes must match the number of criterias.")
+
+        input_texts = [
+            f"{resume}{self.eos_token}{criteria}{self.eos_token}" for resume, criteria in zip(resumes, criterias)
+        ]
+        encoded_inputs = self.tokenizer(
+            input_texts, padding=padding, return_tensors=return_tensors, padding_side=padding_side
         )
-        criteria_tokens = list(
-            map(lambda x: x + [self.eos_token_id], self.tokenizer(criterias)["input_ids"])
-        )
-        input_tokens = [r + c for r, c in zip(resume_tokens, criteria_tokens)]
-        # Pad sequences to the same length with padding on the left, to have the
-        #  leftmost token be the final EOS
-        padded_input_tokens = torch.nn.utils.rnn.pad_sequence(
-            [torch.tensor(t) for t in input_tokens],
-            batch_first=True,
-            padding_value=self.pad_token_id,
-            padding_side="left",
-        )
-        # Compute attention mask (1 for real tokens, 0 for padding)
-        attention_mask = (padded_input_tokens != self.pad_token_id).long()
-        return padded_input_tokens, attention_mask
+        return encoded_inputs
 
     def save_pretrained(self, save_directory: str):
         """
