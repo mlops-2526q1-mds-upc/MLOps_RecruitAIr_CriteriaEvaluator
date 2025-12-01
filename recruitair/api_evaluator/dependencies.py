5935d584582d4aa938cb09ec20f49501aaa1b4f1
viiiiirgi <virgiacireale@gmail.com>
2025-12-01 17:14:24 +0100

    updated api

--- recruitair/api_evaluator/dependencies.py
+++ recruitair/api_evaluator/dependencies.py
@@ -2,13 +2,10 @@ from functools import lru_cache
 from .config import settings
 from .model import DummyEvaluator, TorchMLflowEvaluator, BaseEvaluatorModel
 
-
-@lru_cache(maxsize=1)
+@lru_cache()
 def get_default_model() -> BaseEvaluatorModel:
-    """
-    Returns the loaded model instance. Cached so we only load it once per process.
-    For dev, if settings.use_dummy is True, returns the dummy evaluator.
-    """
-    if settings.use_dummy:
-        return DummyEvaluator()
-    return TorchMLflowEvaluator(model_uri=settings.model_uri, device=settings.device)
+    return TorchMLflowEvaluator(
+        model_uri=settings.model_uri,
+        device=settings.device,
+    )
+    #return DummyEvaluator()
