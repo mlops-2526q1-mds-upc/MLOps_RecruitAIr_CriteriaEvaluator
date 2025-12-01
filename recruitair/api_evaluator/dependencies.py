ebe0a77227d6fb6772ce1da76c6d3a51d004c59f
viiiiirgi <104427159+viiiiirgi@users.noreply.github.com>
2025-12-01 16:18:23 +0000

    Apply automatic changes

--- recruitair/api_evaluator/dependencies.py
+++ recruitair/api_evaluator/dependencies.py
@@ -2,10 +2,11 @@ from functools import lru_cache
 from .config import settings
 from .model import DummyEvaluator, TorchMLflowEvaluator, BaseEvaluatorModel
 
+
 @lru_cache()
 def get_default_model() -> BaseEvaluatorModel:
     return TorchMLflowEvaluator(
         model_uri=settings.model_uri,
         device=settings.device,
     )
-    #return DummyEvaluator()
+    # return DummyEvaluator()
