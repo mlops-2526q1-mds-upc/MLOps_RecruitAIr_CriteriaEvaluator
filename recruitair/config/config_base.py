d7534fc9cfcbd4cf6824618ea0852e511cabcc3a
AimbotParce <marcparcerisa@gmail.com>
2025-12-02 01:11:26 +0100

    Fix the pyproject.toml and add the Dockerfile

--- recruitair/config/config_base.py
+++ recruitair/config/config_base.py
@@ -1,9 +1,11 @@
 """Base project config."""
 
+import logging
 from pathlib import Path
 
 from dotenv import load_dotenv
-from loguru import logger
+
+logger = logging.getLogger(__name__)
 
 # Load environment variables from .env file if it exists
 load_dotenv()
@@ -20,13 +22,3 @@ PROCESSED_DATA_DIR = DATA_DIR / "processed"
 MODELS_DIR = PROJ_ROOT / "models"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
-
-# If tqdm is installed, configure loguru with tqdm.write
-# https://github.com/Delgan/loguru/issues/135
-try:
-    from tqdm import tqdm
-
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
